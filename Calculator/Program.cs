be90bf7fa20c58abe69da915eb559b730cc41b64
Edmar Santos <santosem4@hotmail.com>
2022-03-19 17:40:19 -0300

    Método  Subtração
    Criado método  Subtração
    incluso o comando  Readkey para  programa não finalizar / encerrar automaticamento.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,7 +13,8 @@ namespace Calculator
 
 
 
-         Soma();
+        // Soma();
+         Subtracao();
 
         }
 
@@ -36,6 +37,28 @@ namespace Calculator
            float resultado = v1 + v2;
            System.Console.WriteLine($" O resultado da soma de {v1} + {v2} é = {resultado}");
            System.Console.WriteLine();
+           System.Console.ReadKey();
+
         }
+        static void Subtracao()
+        {
+            System.Console.Clear();
+
+            System.Console.WriteLine("");
+
+            System.Console.Write(" Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            System.Console.Write(" Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("");
+            float resultado = v1 - v2;
+            System.Console.WriteLine($" O resultado da subtração de {v1} - {v2} é = {resultado}");
+            System.Console.WriteLine("");
+            System.Console.ReadKey();
+        }
+    
     }
+    
 }
