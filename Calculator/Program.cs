f06c09bfc932e2bdb7be86152c657b89934363d4
Edmar Santos <santosem4@hotmail.com>
2022-03-19 18:38:09 -0300

    Update Program.cs
    Método  Divisão

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -58,7 +58,27 @@ namespace Calculator
             System.Console.WriteLine("");
             System.Console.ReadKey();
         }
-    
+
+        static void Divisao()
+        {
+            System.Console.Clear();
+
+            System.Console.WriteLine("");
+
+            System.Console.WriteLine(" Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            
+            System.Console.WriteLine(" Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("");
+            float resultado = v1 / v2;
+            System.Console.WriteLine($" O resultado da divisão de {v1} / {v2} é = {resultado}");
+            System.Console.WriteLine("");
+            System.Console.ReadKey();
+        }
+
+
     }
     
 }
