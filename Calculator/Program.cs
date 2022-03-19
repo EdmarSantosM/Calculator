3ad8902baac42063e9c40789cc2e3278f2443f01
Edmar Santos <santosem4@hotmail.com>
2022-03-19 17:24:32 -0300

    Criando Método Soma
    Removido e incluso  a codificação de  somar  dentro  de um método para  organização e fazendo a chamado  direta dentro  do Main.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,10 +6,31 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+
+         Soma();
+
+        }
+
+        static void Soma()
+        {
+
            System.Console.Clear();
            System.Console.WriteLine(" ========================================== ");
            System.Console.WriteLine(" Bem-vindo a sua  Calculadora em Console ");
            System.Console.WriteLine(" ========================================== ");
+
+           System.Console.WriteLine("");
+
+           System.Console.Write(" Digite o primeiro valor: ");
+           float v1 = float.Parse(Console.ReadLine());
+
+           System.Console.Write(" Digite o segundo  valor: ");
+           float v2 = float.Parse(Console.ReadLine());
+
+           System.Console.WriteLine("");
+           float resultado = v1 + v2;
+           System.Console.WriteLine($" O resultado da soma de {v1} + {v2} é = {resultado}");
+           System.Console.WriteLine();
         }
     }
 }
